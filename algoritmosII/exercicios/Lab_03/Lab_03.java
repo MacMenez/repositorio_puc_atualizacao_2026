ba20f0645f92f6f1418cfc20e11d1b31f66beebc
David Ram Menez <62731904+MacMenez@users.noreply.github.com>
2026-03-26 07:07:53 -0300

    Continuando o desenvolvimento do Lab 03
    Necessário trabalhar na parte de exibição, há duplicidades na saída e formatação está divergindo. Necessário terminar os métodos de pesquisa.

--- algoritmosII/exercicios/Lab_03/Lab_03.java
+++ algoritmosII/exercicios/Lab_03/Lab_03.java
@@ -17,22 +17,25 @@ public class Lab_03 {
     while (!(linha_lida = MyIO.readLine()).equals("FIM")) {
       dados_jogo.ler(linha_lida);
       vetor_nao_ordem[indice_vetor] = dados_jogo.clone();
-      vetor_nao_ordem[indice_vetor].imprimir(); // Teste
+      // vetor_nao_ordem[indice_vetor].imprimir(); // Teste
     }
 
     // Ler o vetor ordenado
     while (!(linha_lida = MyIO.readLine(linha_lida)).equals("FIM")) {
       dados_jogo.ler(linha_lida);
       vetor_ordem[indice_vetor] = dados_jogo.clone();
-      vetor_ordem[indice_vetor].imprimir(); // Teste
+      // vetor_ordem[indice_vetor].imprimir(); // Teste
     }
 
     // parte 2: Pesquisa de informações armazenadas no vetor criado na parte 1
     linha_lida = MyIO.readLine();
 
-    vetor_nao_ordem[indice_vetor].imprimir() + "Sequencial: " + pesquisaSequencial(vetor_nao_ordem, linha_lida)  + ", binaria: " + pesquisaBinaria(vetor_ordem, linha_lida) + "\n";
+    while(!(linha_lida = MyIO.readLine()).equals("FIM")){
+      vetor_nao_ordem[indice_vetor].imprimir(); 
+      MyIO.print("Sequencial: " + pesquisaSequencial(vetor_nao_ordem, linha_lida) + ", binaria: " + pesquisaBinaria(vetor_ordem, linha_lida) + "\n");
+    }
     
-    MyIO.readLine(); // MUITO IMPORTANTE
+    // MyIO.readLine(); // MUITO IMPORTANTE
   }
 
     private static int pesquisaSequencial(Jogo[] vetor_nao_ordem, String linha_lida) {
