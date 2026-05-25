5ec2c61ba24564b111828c1ff603e81de751b590
David Ram Menez <dcm.facul@gmail.com>
2026-05-24 22:06:36 -0300

    Insertion Sort - Laboratório 4

--- algoritmosII/exercicios/lab_04/Insercao.java
+++ algoritmosII/exercicios/lab_04/Insercao.java
@@ -5,16 +5,44 @@ class Insercao {
    /**
     * Algoritmo de ordenacao por insercao.
     */
-   public void sort(ArrayList<Jogo> lista_jogos) {
-      int comparacao = 0;
-      int movimentacao = 0;
+   private int comparacao;
+   private int movimentacao;
+   private ArrayList<Jogo> lista_jogos;
+      
+
+   public ArrayList<Jogo> getLista_jogos() {
+      return lista_jogos;
+   }
+
+   public void setLista_jogos(ArrayList<Jogo> lista_jogos) {
+      this.lista_jogos = lista_jogos;
+   }
+
+   public Insercao(ArrayList<Jogo> lista_jogos) {
+      this.comparacao = 0;
+      this.movimentacao = 0;
+      this.lista_jogos = lista_jogos;
+   }
+
+   public int getComparacao() { return comparacao; }
+
+   public void setComparacao(int comparacao) { this.comparacao = comparacao; }
+
+   public int getMovimentacao() { return movimentacao; }
+
+   public void setMovimentacao(int movimentacao) { this.movimentacao = movimentacao; }
+
+   public void sort() {
+      comparacao = 0;
+      movimentacao = 0;
 
       for (int i = 1; i < lista_jogos.size(); i++) {
          Jogo tmp = lista_jogos.get(i);
+         movimentacao++;
+
          int j = i - 1;
 
-         while (j >= 0 && lista_jogos.get(j).compareTo(tmp) > 0) {
-            comparacao++;
+         while (j >= 0 && maior(tmp, lista_jogos.get(j))) {
             lista_jogos.set(j + 1, lista_jogos.get(j));
             movimentacao++;
             j--;
@@ -23,7 +51,28 @@ class Insercao {
          lista_jogos.set(j + 1, tmp);
          movimentacao++;
       }
+   }
 
-      MyIO.println("## INSERTION [COMPARACOES] [" + comparacao + "] [MOVIMENTACOES] [" + movimentacao + "]");
+   public boolean maior(Jogo jogo1, Jogo jogo2) {
+      boolean eMaior = false;
+      
+      if (jogo1.getNome_jogo().compareToIgnoreCase(jogo2.getNome_jogo()) > 0) {
+         comparacao++;
+         eMaior =true;
+      }
+      else if (jogo1.getNome_jogo().compareToIgnoreCase(jogo2.getNome_jogo()) == 0) {
+         if ((jogo1.getPlataforma().compareToIgnoreCase(jogo2.getPlataforma())) > 0) {
+            comparacao += 3;
+            eMaior = true;
+         }
+         else if ((jogo1.getPlataforma().compareToIgnoreCase(jogo2.getPlataforma())) == 0) {
+            if ((jogo1.getVendas_Global() < jogo2.getVendas_Global())) {
+               comparacao += 5;
+               eMaior = true;
+            } else { comparacao += 5; }
+         } else { comparacao += 4; }
+      } else { comparacao++; }
+
+      return eMaior;
    }
 }
